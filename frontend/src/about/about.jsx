d2ecc5cd3c186c5e38c05db90143b0d484a5e628
Fidélis Elias da Silva <fidelis.silva@groupsoftware.com.br>
2018-07-13 08:06:53 -0300

    First commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa História</h2>
+        <p>Nossa história...</p>
+        <h2>Missão e Visão</h2>
+        <p>Missão e visão...</p>
+        <h2>Imprensa</h2>
+        <p>Imprensa...</p>
+    </div>
+)
